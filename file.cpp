8c2edd19d58b277d1efe07daff55a6304c5c08a0
Alexksksss <sasha.litvak02@mail.ru>
2023-03-15 23:00:18 +0700

    commit3File

---
+++ file.cpp
@@ -0,0 +1,36 @@
+class StateFile{
+
+public:
+StateFile(Qstring );
+StateFile(){
+
+//???
+}
+
+private:
+Qstring FileName;
+qint64 size;
+bool isExist;
+
+
+};
+
+
+class FileMonitor{
+
+public:
+FileMonitor();
+bool AddFile(Qstring FN);//статус добавления файла в наблюдатель. 0 если объект уже под наблюдением
+bool DelFile(Qstring FN);//0 если не можем удалить (если его нет изначально)
+
+
+??CheckStatus();
+
+private:
+//FileState *objects;
+//FileState **objects;
+
+//Контейнеры
+//QVector <FileState> objects;
+//QSet <FileState> objects;
+};
--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World! its test222222222222" << endl;
+    cout << "Hello World! its test222" << endl;
     return 0;
 }
+
+void main(){
+FileMonitor monitor;
+monitor.AddFile('a.txt');
+monitor.AddFile('b.txt');
+monitor.AddFile('c.txt');
+monitor.AddFile('d.txt');
+
+
+while(doCheckObj){
+
+monitor.CheckStatus();
+
+}
+}
+
+
